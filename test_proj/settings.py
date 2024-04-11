8f932aed6e06e7753c3c7d65e8aad4aea648222c
Tiaw999 <tia.walker@spartans.ut.edu>
2024-04-10 22:02:49 -0400

    Database backup

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-+^eni517cej37ulh5z2bu8=eku3qwtza##aebf8v5dostd+n3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']#,'127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
@@ -127,9 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
